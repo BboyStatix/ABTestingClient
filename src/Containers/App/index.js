25424b6d7dd3cf70b8d184f4358eac02f19738f0
BboyStatix <statix231@hotmail.com>
2020-02-10 09:58:38 +0800

    Create entry point into app

---
+++ src/Containers/App/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { gql } from 'apollo-boost'
+import {useQuery} from "@apollo/react-hooks";
+import AppCatalogue from "../AppCatalogue";
+import {VariantContext} from "../../Contexts";
+import Error from "../../Components/Error";
+import {Router} from "@reach/router";
+import AppDetail from "../AppDetail";
+import Layout from "../../Layouts/DefaultLayout";
+
+const GET_VARIANT = gql`
+  {
+    variant
+  }
+`
+
+const App = () => {
+  const {loading, error, data = {}} = useQuery(GET_VARIANT)
+  const { variant } = data
+
+  if(loading) return 'Loading...'
+
+  if(error) return <Error />
+
+  if(variant) {
+    return (
+        <VariantContext.Provider value={variant}>
+          <Layout>
+            <Router>
+              <AppCatalogue path={'/'} />
+              <AppDetail path={'app/:id'} />
+            </Router>
+          </Layout>
+        </VariantContext.Provider>
+    )
+  }
+  else {
+    return null
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import App from './Containers/App';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from '@apollo/react-hooks';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    uri: `${process.env.REACT_APP_SERVER_URL}`
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+)
