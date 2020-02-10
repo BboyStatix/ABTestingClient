dfaa904d7df211e9d754f5fdc4467aaeb777c651
BboyStatix <statix231@hotmail.com>
2020-02-10 11:30:26 +0800

    Add loading and error handler to AppCatalogueA

--- src/Containers/AppCatalogue/A/index.js
+++ src/Containers/AppCatalogue/A/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {gql} from "apollo-boost";
 import {useQuery} from "@apollo/react-hooks";
-import {Link} from "@reach/router";
 import styles from "./Styles";
 import ListItem from "./ListItem";
+import Error from "../../../Components/Error";
 
 const GET_APPS = gql`
     {
@@ -19,6 +19,10 @@ const GET_APPS = gql`
 const A = () => {
     const {loading, error, data = {}} = useQuery(GET_APPS)
     const { apps } = data
+
+    if(loading) return "Loading..."
+
+    if(error) return <Error />
 
     if(apps) {
         return (
