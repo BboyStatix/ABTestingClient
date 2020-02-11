ef0addd21292fdda784156e970b0359cda1751bc
BboyStatix <statix231@hotmail.com>
2020-02-11 22:16:36 +0800

    Create test for loadVariantComponent

---
+++ src/HOCs/tests/loadVariantComponent.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import loadVariantComponent from "../loadVariantComponent";
+
+describe('loadVariantComponent', function () {
+    //TODO
+    test('it returns the correct component for the particular variant', () => {
+        //mockVariant('A')
+    })
+});
